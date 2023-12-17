901e4ffeec4b3b66f78945109548561e6f3afb58
soymilk0211 <145407839+soymilk0211@users.noreply.github.com>
2023-12-18 04:49:37 +0800

    Add files via upload

--- HW5/main2.c
+++ HW5/main2.c
@@ -1,33 +1,30 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
-int main() {
+int main(){
     double pi = 4.0;
-    int k = 1;
-    int x = 0;
-    for (int i=3 ; i<=1000000 ; i+=2){
+    int k = 0;
+    int i = 3;
+    for( i=3;i<=100000;i+=2){
         if (k==0){
-            pi+=(4.0/i);
-            k =1;
+            pi-=(4/i);
+            k=1;
         }
-        else if (k==1){
-            pi-=(4.0/i);
-            k =0;
+        if(k==1){
+            pi+=(4/i);
+            k=0;
         }
-        int ipi = pi*1000000;
-        if (ipi == 3141592){
-            x=i;
+        if(pi==3.14159){
             break;
-            
         }
     }
-    printf("%d %.5f",x,pi);
+    printf("%d",i);
     return 0;
 }
